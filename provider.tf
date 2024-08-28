22f35a9709d9621cef38aa2a12380da4e304cd7e
huzaifa haroun <huzaifa.haroun@gmail.com>
2024-08-28 13:55:37 +0200

    first commit

--- provider.tf
+++ provider.tf
@@ -1,10 +1,11 @@
 provider "azurerm" {
   features {}
+  subscription_id = "68bf633f-2654-4d8b-845e-c0d8d5f53cd7"
 }
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "noorterraformstatest"
+    storage_account_name = "noorterraformstatest1"
     container_name       = "terraform-state"
     key                  = "noor-azure-web-iac.terraform.tfstate"
     # Can also be set via `ARM_ACCESS_KEY` environment variable.
